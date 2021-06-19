6a191540dadea385c0153d1440696448ba9f1d69
manelzada <emmanoelsveiga@gmail.com>
2021-06-19 13:22:19 -0300

    Mudança 2

--- Teste.java
+++ Teste.java
@@ -5,7 +5,7 @@ public class Teste{
     public static void main (String[]args){
         Scanner entrada = new Scanner(System.in);
         String nome = entrada.next();
-        System.out.print(nome);
+        System.out.println(nome);
         entrada.close();
     }
 }
