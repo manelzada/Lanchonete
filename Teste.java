20d838c9353c33288e8566d8e837f4221d5cfae0
manelzada <emmanoelsveiga@gmail.com>
2021-06-19 13:14:51 -0300

    Mudança feita

--- Teste.java
+++ Teste.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class Teste{
 
     public static void main (String[]args){
-        System.out.print("alooo");
+        Scanner entrada = new Scanner(System.in);
+        String nome = entrada.next();
+        System.out.print(nome);
+        entrada.close();
     }
 }
